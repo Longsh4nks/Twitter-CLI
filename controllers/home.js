e91b5e1fddf82fd7f41274e41bf0b3656590d3b2
N3UR0515 <longcoua@gmail.com>
2018-03-14 22:26:16 -0700

    Added Comments

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,8 @@
 var post = require("../post/post.js");
 var stream = require("../stream/stream.js");
 
+
+//This is the controller method.
 module.exports = (meth, args) => {
     if (args == null || meth == null){
         console.log("Incorrect format")
